016888390a0cb1513be36b2fd3b1f3e152754d2b
mr147sec <busiyg@163.com>
2021-04-28 10:27:16 +0800

    Update

--- Assets/SequenceViewer/Scrips/MainManager.cs
+++ Assets/SequenceViewer/Scrips/MainManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
+    public string folderPath;
+    public List<GameObject> models;
+    public int maxSize;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // ReadFiles
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ReadFiles() {
+        for (int i=0;i< maxSize;i++) { 
+        
+        }
     }
 }
