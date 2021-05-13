fbdee665bde509a4aa792c66a2f744cdf99f7e53
mr147sec <busiyg@163.com>
2021-05-13 17:19:44 +0800

    Update

--- Assets/SequenceViewer/Scrips/MainManager.cs
+++ Assets/SequenceViewer/Scrips/MainManager.cs
@@ -9,119 +9,169 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public ModelSequenceItem currentModelSequenceItem;
     public List<ModelSequenceItem> ModelSequenceItemList;
     public string folderPath;
     public bool isPlaying;
-    public Material baseMaterial;
-    public Transform modelPos;
-    public Transform modelParent;
-    public int frameCount;
-    private int startIndex;
-    public int currentIndex;
-    public float frameRate;
+    public bool isOpend;
+    public int currentFrameIndex;
+    public int frameRate;
     private float interval;
     private AssetLoaderOptions assetLoaderOptions;
     public Text frameText;
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
+
+    public bool needBaseMap;
+    public bool needMaskMap;
+    public bool needNormalMap;
+
+    public string baseMapAdditionName;
+    public string maskMapAdditionName;
+    public string normalMapAdditionName;
+
+    public int fileMaxSize;
+    public InputField inputField;
+    public InputField frameRateInputField;
+    public GameObject loadingUI;
+    public Text loadingText;
+    public Slider slider;
+
+    public bool isSliderSelect;
+
     // Start is called before the first frame update
     void Start()
     {
-        interval = 1f / frameRate;
-        // ReadFiles
+        
         assetLoaderOptions = AssetLoader.CreateDefaultLoaderOptions();
         ModelSequenceItemList = new List<ModelSequenceItem>();
+
+        inputField.onValueChanged.AddListener((str)=> {
+            folderPath = str;
+        });
+
+        frameRateInputField.onValueChanged.AddListener((value) => {
+            try
+            {
+                frameRate = int.Parse(value);
+            }
+            catch { 
+            
+            }
+        });
+
+        slider.onValueChanged.AddListener((value) => {
+            if (isOpend&& isSliderSelect)
+            {
+                if (isPlaying)
+                {
+                    isPlaying = !isPlaying;
+                }
+           
+                currentFrameIndex = (int)value;
+                UpdateModel(currentFrameIndex);
+            }
+        });
+        frameRate = 22;
+        interval = 1f / frameRate;
+    }
+
+    public void OnClickSlider()
+    {      
+        isSliderSelect = true;
+        if (isOpend) {
+            if (isPlaying)
+            {
+                isPlaying = !isPlaying;
+            }
+         
+            currentFrameIndex = (int)slider.value;
+            UpdateModel(currentFrameIndex);
+        }  
+    }
+
+    public void OnReleaseSlider()
+    {
+        isSliderSelect = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isOpend) {
+            slider.maxValue = fileMaxSize - 1;
+        }
+    }
 
+    public void OnClickReadFile() {
+        loadingUI.SetActive(true);
+        Invoke("ReadFiles",0.2f);
     }
 
     public void ReadFiles()
     {
-        GetFirstIndex();
-        Debug.LogWarning("ReadFiles");
-        for (int i = startIndex; i < startIndex+frameCount; i++)
+        ModelSequenceItemList.Clear();
+        isPlaying = false;
+        isOpend = false;
+        folderPath += @"\";
+        string[] filesNames = Directory.GetFiles(folderPath, "*.obj");
+        fileMaxSize = filesNames.Length;
+        for (int i = 0; i < filesNames.Length; i++)
         {
-            string indexStr = i.ToString().PadLeft(6,'0');
-            string modelFilePath = $"{folderPath}{indexStr}.obj";
-            string baseMapPath = $"{folderPath}{indexStr}_material_0_BaseMap.png";
-            string maskMapPath = $"{folderPath}{indexStr}_material_0_MaskMap.png";
-            string normalMapPath = $"{folderPath}{indexStr}_material_0_Normal.png";
+            string objFilePath = filesNames[i];
 
-            Debug.LogWarning(modelFilePath);
-            bool isModelExist = IsFileExist(modelFilePath);
-            if (isModelExist) {
-                // Model
-                var obj = AssetLoader.LoadModelFromFileNoThread(modelFilePath);             
-               // obj.RootGameObject.transform.eulerAngles = new Vector3(180, 0, 0);
-              //  obj.RootGameObject.transform.position = modelPos.transform.position;
-             //   obj.RootGameObject.transform.parent = modelParent;
+            if (IsFileExist(objFilePath))
+            {
+                loadingText.text = $"¼ÓÔØÖÐ£¬ÇëÉÔµÈ£º{i}/{fileMaxSize}";
+                string indexStr = objFilePath.Replace(".obj", "");
+                var obj = AssetLoader.LoadModelFromFileNoThread(objFilePath);
                 ModelSequenceItem modelSequenceItem = new ModelSequenceItem();
-             //   modelSequenceItem.obj = obj.RootGameObject.transform.GetChild(0).gameObject;
-                //modelSequenceItem.meshRenderer = modelSequenceItem.obj.GetComponent<MeshRenderer>();
-                //modelSequenceItem.meshRenderer.material = baseMaterial;
                 modelSequenceItem.mesh = obj.RootGameObject.transform.GetChild(0).gameObject.GetComponent<MeshFilter>().mesh;
-                modelSequenceItem.mesh.name = indexStr;
+                modelSequenceItem.mesh.name = objFilePath;
 
-
-                if (IsFileExist(baseMapPath))
+                if (needBaseMap)
                 {
-                    StartCoroutine(DoWebRequestGetTexture(baseMapPath, (tex) => {
-                        tex.name = indexStr;
-                        modelSequenceItem.baseMap = tex;
-                    }));
+                    string baseMapPath = $"{indexStr}{baseMapAdditionName}";
+                    if (IsFileExist(baseMapPath))
+                    {
+                        StartCoroutine(DoWebRequestGetTexture(baseMapPath, (tex) =>
+                        {
+                            tex.name = baseMapPath;
+                            modelSequenceItem.baseMap = tex;
+                        }));
+                    }
                 }
 
-                if (IsFileExist(maskMapPath))
+                if (needMaskMap)
                 {
-                    StartCoroutine(DoWebRequestGetTexture(maskMapPath, (tex) => {
-                        tex.name = indexStr;
-                        modelSequenceItem.maskMap = tex;
-                    }));
+                    string maskMapPath = $"{indexStr}{maskMapAdditionName}";
+                    if (IsFileExist(maskMapPath))
+                    {
+                        StartCoroutine(DoWebRequestGetTexture(maskMapPath, (tex) => {
+                            tex.name = maskMapPath;
+                            modelSequenceItem.maskMap = tex;
+                        }));
+                    }
                 }
 
-                if (IsFileExist(normalMapPath))
+                if (needNormalMap)
                 {
-                    StartCoroutine(DoWebRequestGetTexture(normalMapPath, (tex) => {
-                        tex.name = indexStr;
-                        modelSequenceItem.normalMap = tex;
-                    }));
+                    string normalMapPath = $"{indexStr}{normalMapAdditionName}";
+                    if (IsFileExist(normalMapPath))
+                    {
+                        StartCoroutine(DoWebRequestGetTexture(normalMapPath, (tex) => {
+                            tex.name = normalMapPath;
+                            modelSequenceItem.normalMap = tex;
+                        }));
+                    }
                 }
-             //   modelSequenceItem.obj.SetActive(false);
+
                 ModelSequenceItemList.Add(modelSequenceItem);
-
                 Destroy(obj.RootGameObject);
             }
-            else {
-                Debug.LogError("file not exsit");
-            }
         }
-        currentIndex = 0;
-       // UpdateModelByIndex(currentIndex);
-    }
-
-    public void GetFirstIndex() {
-
-        string[] txtFiles = Directory.GetFiles(folderPath);
-        frameCount = txtFiles.Length / 4;
-
-        for (int i = 0; i < 100000; i++) {
-            string indexStr = i.ToString().PadLeft(6, '0');
-            string modelFilePath = $"{folderPath}{indexStr}.obj";
-
-            if (IsFileExist(modelFilePath)) {
-                startIndex = i;
-                Debug.LogWarning("startIndex is:"+ startIndex);
-                break;
-            }
-            else { 
-            
-            }
-        }
+        isOpend = true; 
+        loadingUI.SetActive(false);
+        PlayOrPause();
     }
 
     public void PlayOrPause()
@@ -134,9 +184,7 @@ public class MainManager : MonoBehaviour
         {
             isPlaying = !isPlaying;
             Play();
-        }
-
-        
+        }       
     }
 
     public void Play()
@@ -145,94 +193,41 @@ public class MainManager : MonoBehaviour
     }
 
     public IEnumerator DoPlay() {
-        while (isPlaying) {
-            interval = 1 / frameRate;
-            if (currentIndex> ModelSequenceItemList.Count-1) {
-                currentIndex = 0;
+        if (isOpend) {
+            while (isPlaying)
+            {
+                interval = 1 / (float)frameRate;
+                if (currentFrameIndex > ModelSequenceItemList.Count - 1)
+                {
+                    currentFrameIndex = 0;
+                }
+                UpdateModel(currentFrameIndex);
+          
+                currentFrameIndex += 1;
+                yield return new WaitForSeconds(interval);
             }
-            UpdateModel(currentIndex);
-            //UpdateModelByIndex(currentIndex);
-            frameText.text = currentIndex.ToString();
-            currentIndex += 1;
-            yield return new WaitForSeconds(interval);
-        }
+        }    
     }
-
-    //public void UpdateModelByIndex(int index) {
-    //    //if (currentModelSequenceItem != null)
-    //    //{
-    //    //    currentModelSequenceItem.obj.SetActive(false);
-    //    //}
-
-    //    baseMaterial.mainTexture = ModelSequenceItemList[currentIndex].baseMap;
-    //    //baseMaterial.SetTexture("_MaskMap", ModelSequenceItemList[currentIndex].maskMap);
-    //    //baseMaterial.SetTexture("_NormalMap", ModelSequenceItemList[currentIndex].normalMap);
-    // //   ModelSequenceItemList[currentIndex].obj.SetActive(true);
-    //    currentModelSequenceItem = ModelSequenceItemList[currentIndex];
-    //}
 
     public void UpdateModel(int index)
     {
-        meshRenderer.material.mainTexture = ModelSequenceItemList[currentIndex].baseMap;
-        meshRenderer.material.SetTexture("_MaskMap", ModelSequenceItemList[currentIndex].maskMap);
-        meshRenderer.material.SetTexture("_NormalMap", ModelSequenceItemList[currentIndex].normalMap);
-        meshFilter.mesh = ModelSequenceItemList[currentIndex].mesh;
-        //ModelSequenceItemList[currentIndex].obj.SetActive(true);
-        //currentModelSequenceItem = ModelSequenceItemList[currentIndex];
+        frameText.text = currentFrameIndex.ToString();
+        slider.value = currentFrameIndex;
+
+        meshRenderer.material.mainTexture = ModelSequenceItemList[currentFrameIndex].baseMap;
+        meshRenderer.material.SetTexture("_MaskMap", ModelSequenceItemList[currentFrameIndex].maskMap);
+        meshRenderer.material.SetTexture("_NormalMap", ModelSequenceItemList[currentFrameIndex].normalMap);
+        meshFilter.mesh = ModelSequenceItemList[currentFrameIndex].mesh;
     }
 
     [System.Serializable]
     public class ModelSequenceItem
     {
-       // public GameObject obj;
-        //public MeshRenderer meshRenderer;
         public Mesh mesh;
         public Texture baseMap;
         public Texture normalMap;
         public Texture maskMap;
     }
-
-    ///// <summary>
-    ///// Called when any error occurs.
-    ///// </summary>
-    ///// <param name="obj">The contextualized error, containing the original exception and the context passed to the method where the error was thrown.</param>
-    //private void OnError(IContextualizedError obj)
-    //{
-    //    Debug.LogError($"An error occurred while loading your Model: {obj.GetInnerException()}");
-    //}
-
-    ///// <summary>
-    ///// Called when the Model loading progress changes.
-    ///// </summary>
-    ///// <param name="assetLoaderContext">The context used to load the Model.</param>
-    ///// <param name="progress">The loading progress.</param>
-    //private void OnProgress(AssetLoaderContext assetLoaderContext, float progress)
-    //{
-    //    Debug.Log($"Loading Model. Progress: {progress:P}");
-    //}
-
-    ///// <summary>
-    ///// Called when the Model (including Textures and Materials) has been fully loaded, or after any error occurs.
-    ///// </summary>
-    ///// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
-    ///// <param name="assetLoaderContext">The context used to load the Model.</param>
-    //private void OnMaterialsLoad(AssetLoaderContext assetLoaderContext)
-    //{
-    //    Debug.Log("Materials loaded. Model fully loaded.");
-    //}
-
-    ///// <summary>
-    ///// Called when the Model Meshes and hierarchy are loaded.
-    ///// </summary>
-    ///// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
-    ///// <param name="assetLoaderContext">The context used to load the Model.</param>
-    //private void OnLoad(AssetLoaderContext assetLoaderContext)
-    //{
-    //    assetLoaderContext.RootGameObject.transform.eulerAngles = new Vector3(180, 0, 0);
-    //    assetLoaderContext.RootGameObject.transform.position = modelPos.transform.position;
-    //    assetLoaderContext.RootGameObject.transform.parent = modelParent;
-    //    Debug.Log("Model loaded. Loading materials.");
-    //}
 
     public bool IsFileExist(string path)
     {
@@ -245,7 +240,7 @@ public class MainManager : MonoBehaviour
         {
             yield return unityWebRequest.SendWebRequest();
 
-            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            if (unityWebRequest.result == UnityWebRequest.Result.ConnectionError || unityWebRequest.result == UnityWebRequest.Result.ProtocolError)
             {
                 //Debug.Log("error");
                 if (onError != null)
@@ -263,21 +258,28 @@ public class MainManager : MonoBehaviour
     }
 
     public void Next() {
-        currentIndex += 1;
-        if (currentIndex > ModelSequenceItemList.Count - 1)
-        {
-            currentIndex = 0;
+        if (isPlaying) {
+            isPlaying = false;
         }
-        UpdateModel(currentIndex);       
+        currentFrameIndex += 1;
+        if (currentFrameIndex > ModelSequenceItemList.Count - 1)
+        {
+            currentFrameIndex = 0;
+        }
+        UpdateModel(currentFrameIndex);       
     }
 
     public void Last()
     {
-        currentIndex -= 1;
-        if (currentIndex <0)
+        if (isPlaying)
         {
-            currentIndex = ModelSequenceItemList.Count - 1;
+            isPlaying = false;
         }
-        UpdateModel(currentIndex);       
+        currentFrameIndex -= 1;
+        if (currentFrameIndex <0)
+        {
+            currentFrameIndex = ModelSequenceItemList.Count - 1;
+        }
+        UpdateModel(currentFrameIndex);       
     }
 }
